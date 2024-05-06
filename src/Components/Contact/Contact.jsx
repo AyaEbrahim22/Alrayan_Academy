3e23e9d46b60c4eeac503b19ad468ff321ff0097
Aya Ebrahim <Aya.ebrahim.190900@gmail.com>
2024-05-07 01:14:46 +0300

    RUpdate1

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,7 +10,7 @@ export default function Contact() {
     return <>
         <div className={style.contactComponent}>
             <div className='container'>
-                <div className='row text-center gx-5 gy-4 mb-5'>
+                <div className='row text-center gx-4 gy-4 mb-5'>
                     <div className='col-md-4'>
                         <div className={style.contactCard}>
                             <h3>Email Us</h3>
@@ -85,7 +85,7 @@ export default function Contact() {
                                 Get access to the convenience and flexibility of learning the Quran online from the comfort of your home. Our dedicated Quran tutors are ready to guide and support you in your Quranic studies. Join our community of learners and become part of the spiritual Qurani learning journey.
                             </p>
 
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.1760797492466!2d31.332694081661582!3d30.06048691023684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583e65c130f951%3A0xd2b9714b75fc3cb5!2sAbbas%20El-Akkad%2C%20Al%20Manteqah%20Al%20Oula%2C%20Nasr%20City%2C%20Cairo%20Governorate!5e0!3m2!1sen!2seg!4v1714344024027!5m2!1sen!2seg" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="position"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.1760797492466!2d31.332694081661582!3d30.06048691023684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583e65c130f951%3A0xd2b9714b75fc3cb5!2sAbbas%20El-Akkad%2C%20Al%20Manteqah%20Al%20Oula%2C%20Nasr%20City%2C%20Cairo%20Governorate!5e0!3m2!1sen!2seg!4v1714344024027!5m2!1sen!2seg" className='w-100' height={450} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="position"></iframe>
                         </div>
                     </div>
                 </div>
